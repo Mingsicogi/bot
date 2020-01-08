cbc07446da37c4df10bdb41ad13f50a369fe806d
Mingsicogi <codedoctor119@gmail.com>
2020-01-09 02:15:12 +0900

    initial commit

--- src/main/java/my/mins/bot/BotApplication.java
+++ src/main/java/my/mins/bot/BotApplication.java
@@ -6,16 +6,25 @@ import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
-import lombok.extern.slf4j.Slf4j;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-@Slf4j
+@LineMessageHandler
 public class BotApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(BotApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(BotApplication.class, args);
+		SpringApplication app = new SpringApplication(BotApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		String phaseCd = env.getRequiredProperty("line.bot.channel-token"); //실행환경  System.out.println(phaseCd);
+
+		log.info(phaseCd);
 	}
 
 	@EventMapping
