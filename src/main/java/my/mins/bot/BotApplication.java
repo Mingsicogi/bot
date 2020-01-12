efba9b26913b5dbcd05f555ca6a6ca31a97d4be0
Mingsicogi <codedoctor119@gmail.com>
2020-01-12 19:45:06 +0900

    Message handling dev

--- src/main/java/my/mins/bot/BotApplication.java
+++ src/main/java/my/mins/bot/BotApplication.java
@@ -3,9 +3,11 @@ package my.mins.bot;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.event.source.Source;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineBotMessages;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,15 +29,15 @@ public class BotApplication {
 		log.info(phaseCd);
 	}
 
-	@EventMapping
-	public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
-		log.info("event: " + event);
-		final String originalMessageText = event.getMessage().getText();
-		return new TextMessage(originalMessageText);
-	}
+
 
 	@EventMapping
-	public void handleDefaultMessageEvent(Event event) {
-		System.out.println("event: " + event);
+	public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+		log.info("#### event : " + event);
+
+		log.info("#### source : " + event.getSource());
+
+		final String originalMessageText = event.getMessage().getText();
+		return new TextMessage("지금은 개발중~!");
 	}
 }
